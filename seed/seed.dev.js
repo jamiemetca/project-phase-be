d3b4c278900b55b7db3a0fb651d4b6739604d446
antariess <velislava.n.georgieva@gmail.com>
2018-07-25 12:24:20 +0100

    fixes linting in seed folder

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -1,16 +1,12 @@
-const { seedDB } = require("./seed");
-const mongoose = require("mongoose");
-const rawData = require("./testData");
-const { DB_URL } = require("../config");
+const mongoose = require('mongoose');
+const { seedDB } = require('./seed');
+const rawData = require('./testData');
+const { DB_URL } = require('../config');
 
 mongoose
   .connect(DB_URL)
-  .then(() => {
-    return seedDB(rawData);
-  })
-  .then(() => {
-    return mongoose.disconnect();
-  })
+  .then(() => seedDB(rawData))
+  .then(() => mongoose.disconnect())
   .then(() => {
     console.log(`Successfully disconnected from ${DB_URL}`);
   })
--- seed/seed.js
+++ seed/seed.js
@@ -1,21 +1,17 @@
-const mongoose = require("mongoose");
-const { User, Journey } = require("../models");
-const { createUserLookup, formatJourneyData } = require("../utils");
+const mongoose = require('mongoose');
+const { User, Journey } = require('../models');
+const { createUserLookup, formatJourneyData } = require('../utils');
 
-const seedDB = ({ userData, journeyData }) => {
-  return mongoose.connection
-    .dropDatabase()
-    .then(() => {
-      return User.insertMany(userData);
-    })
-    .then(userDocs => {
-      const userLookup = createUserLookup(userDocs, journeyData);
-      return Promise.all([
-        userDocs,
-        Journey.insertMany(formatJourneyData(journeyData, userLookup))
-      ]);
-    })
-    .catch(console.log);
-};
+const seedDB = ({ userData, journeyData }) => mongoose.connection
+  .dropDatabase()
+  .then(() => User.insertMany(userData))
+  .then((userDocs) => {
+    const userLookup = createUserLookup(userDocs, journeyData);
+    return Promise.all([
+      userDocs,
+      Journey.insertMany(formatJourneyData(journeyData, userLookup)),
+    ]);
+  })
+  .catch(console.log);
 
 module.exports = { seedDB };
