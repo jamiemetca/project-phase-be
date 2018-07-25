0cb7d2b0b25b2663cb3093c7259dd6ac0ffca8e6
antariess <velislava.n.georgieva@gmail.com>
2018-07-25 11:48:47 +0100

    fixes linting in test file and journeys.js

--- controllers/journeys.js
+++ controllers/journeys.js
@@ -1,29 +1,29 @@
-const { Journey } = require('../models')
+const { Journey } = require('../models');
 
 const getAllJourneys = (req, res, next) => {
   Journey.find()
-    .then(journeys => {
-      res.send({ journeys })
+    .then((journeys) => {
+      res.send({ journeys });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
 const getJourneyById = (req, res, next) => {
   Journey.findOne(req.params)
-    .then(journey => {
-      if(journey === null) throw next({status:404, message: 'Page not found'})
-      res.send({ journey })
+    .then((journey) => {
+      if (journey === null) throw next({ status: 404, message: 'Page not found' });
+      res.send({ journey });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
 const addJourney = (req, res, next) => {
-  const journey = new Journey(req.body)
+  const journey = new Journey(req.body);
   journey.save()
     .then((newJourney) => {
-      res.status(201).send({newJourney})
+      res.status(201).send({ newJourney });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
-module.exports = { getAllJourneys, getJourneyById, addJourney }
+module.exports = { getAllJourneys, getJourneyById, addJourney };
