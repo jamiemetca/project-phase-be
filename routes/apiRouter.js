9f31764e1b585412ff0150464669ddd0d2e1017e
antariess <velislava.n.georgieva@gmail.com>
2018-08-02 16:56:40 +0100

    html with routes

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,12 +2,10 @@ const apiRouter = require('express').Router();
 const { usersRouter } = require('./usersRouter');
 const { journeysRouter } = require('./journeysRouter');
 
-
-// main page, create index file with list of apis, if ejs need to do view ingine as ejs in app
-// apiRouter.route('/')
-//     .get((req, res, next) => {
-//         res.render('public/index')
-//     })
+apiRouter.reoute('/')
+  .get((req, res, next) => {
+    res.render('public/index');
+  });
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/journeys', journeysRouter);
